74eb9a9364fc673bf4e6cd16af8eae142cc7fbbd
magralo <magralo@gmail.com>
2023-06-06 03:02:45 -0500

    add readme to pypi

--- setup.py
+++ setup.py
@@ -1,8 +1,15 @@
 from distutils.core import setup
+
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+
 setup(
   name = 'easydags',         # How you named your package folder (MyLib)
   packages = ['easydags'],   # Chose the same as "name"
-  version = '0.0.2',      # Start with a small number and increase it with every change you make
+  version = '0.0.3',      # Start with a small number and increase it with every change you make
   description = 'Dags made easy',   # Give a short description about your library
   author = 'Mateo Graciano',                   # Type in your name
   author_email = 'magralo@gmail.com',      # Type in your E-Mail
@@ -13,6 +20,8 @@ setup(
           'matplotlib==3.6.3',
           'pyvis==0.3.1'
       ],
+  long_description=long_description,
+  long_description_content_type='text/markdown',
   classifiers=[
     'Development Status :: 3 - Alpha',      
   ],
